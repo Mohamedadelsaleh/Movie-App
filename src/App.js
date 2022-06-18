8504aba6d7787fdf384fce5d02f479959f5944ac
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-06-18 19:46:01 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Home/HomePages';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
+      <Footer />
     </Router>
     </>
   );
