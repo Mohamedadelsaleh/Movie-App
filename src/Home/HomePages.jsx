88badddf858d47062c3d5111723cfd653309fdd7
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-06-18 19:00:43 +0200

    First finish for Upcomming

--- src/Home/HomePages.jsx
+++ src/Home/HomePages.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import Homes from '../Components/Homes/Homes'
+import Upcomming from '../Components/Upcomming/Upcomming'
+import {upcome} from '../StaticData'
 
 const Home = () => {
 
+    const [items, setItems] = useState(upcome)
 
     return (
         <>
             <Homes />
+            <Upcomming items={items} title='Upcomming Movies' />
         </>
     )
 }
