38633235b1c648a595155ac63aa9554ddaefacac
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-06-18 17:11:43 +0200

    Finishing Slider

--- src/Components/Homes/HomeCard.jsx
+++ src/Components/Homes/HomeCard.jsx
@@ -1,60 +1,61 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import playimg from '../../assets/images/icons/play.png'
-import playBTN from '../../assets/images/icons/play.png'
- 
-const HomeCard = ({item:{id, cover, name, rating, time, desc, starring, genres, tags, video} }) => {
+
+
+const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genres, tags, video } }) => {
     return (
         <>
-            <div className="box">
-                <div className="coverImage">
-                    <img src={cover} alt="" />
-                </div>
-                <div className="content flex">
-                    <div className="details row">
-                        <h1>{name}</h1>
-                        <div className="rating flex">
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star-half"></i>
-                        </div>
-                        <label>{rating}</label>
-                        <span>GP</span>
-                        <label>{time}</label>
-                    </div>
-                    <p>{desc}</p>
-                    <div className='cast'>
-                        <h4>
-                            <span>Starring</span>
-                            {starring}
-                        </h4>
-                        <h4>
-                            <span>Genres</span>
-                            {genres}
-                        </h4>
-                        <h4>
-                            <span>Tags</span>
-                            {tags}
-                        </h4>
-                    </div>
-                    <button className="primary-btn">
-                        <i className="fa fa-play"></i>PLAY NOW
-                    </button>
-                    <div className='button row'>
-                        <Link to={`/singlepage/${id}`}>
-                            <button>
-                                <div className='img'>
-                                    <img src={playimg} alt='play' />
-                                    <img src={playBTN} alt='' />
-                                </div>
-                                WATCH TRILER
-                            </button>
-                        </Link>
-                    </div>
-                </div>
+        <div className='box'>
+            <div className='coverImage'>
+            <img src={cover} alt='' />
             </div>
+            <div className='content flex'>
+            <div className='details row'>
+                <h1>{name}</h1>
+                <div className='rating flex'>
+                <div className='rate'>
+                    <i className='fas fa-star'></i>
+                    <i className='fa fa-star'></i>
+                    <i className='fa fa-star'></i>
+                    <i className='fa fa-star'></i>
+                    <i className='fa fa-star-half'></i>
+                </div>
+                <label>{rating}(Imdb)</label>
+                <span>GP</span>
+                <label>{time}</label>
+                </div>
+                <p>{desc}</p>
+                <div className='cast'>
+                <h4>
+                    <span>Starring </span>
+                    {starring}
+                </h4>
+                <h4>
+                    <span>Genres </span>
+                    {genres}
+                </h4>
+                <h4>
+                    <span>Tags </span>
+                    {tags}
+                </h4>
+                </div>
+                <button className='primary-btn'>
+                <i className='fas fa-play'></i> PLAY NOW
+                </button>
+            </div>
+            <div className='palyButton row'>
+                <Link to={`/singlepage/${id}`}>
+                <button>
+                    <div className='img'>
+                    <img src='./images/icons/play-button.png' alt='' />
+                    <img src='./images/icons/play (4).png' className='change' alt='' />
+                    </div>
+                    WATCH TRAILER
+                </button>
+                </Link>
+            </div>
+            </div>
+        </div>
         </>
     )
 }
--- src/Components/Homes/HomeOne.jsx
+++ src/Components/Homes/HomeOne.jsx
@@ -1,15 +1,53 @@
 import React from 'react'
 import HomeCard from './HomeCard'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const SmapleNextArrow = (props) => {
+    const {onClick} = props;
+    return (
+        <div className="control-btn" onClick={onClick}>
+            <button className='next'>
+                <i className='fas fa-chevron-right'></i>
+            </button>
+        </div>
+    )
+}
+
+const SmaplePrevArrow = (props) => {
+    const {onClick} = props;
+    return (
+        <div className="control-btn" onClick={onClick}>
+            <button className='prev'>
+                <i className='fas fa-chevron-left'></i>
+            </button>
+        </div>
+    )
+}
 
 const HomeOne = ({items}) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: <SmapleNextArrow />,
+        prevArrow: <SmaplePrevArrow />,
+
+    };
+
     return (
         <>
             <div className="homeContainer">
-                {items.map((item) => 
-                    (
-                        <HomeCard key={item.id} item={item}/>
-                    )
-                )}
+                <Slider {...settings}>
+                    {items.map((item) => 
+                        (
+                            <HomeCard key={item.id} item={item}/>
+                        )
+                    )}
+                </Slider>
             </div>
         </>
     )
